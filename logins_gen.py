168cd0fb09a4cc9f00225af6ccdcbac9c467f8bb
Szymon Majewski <smajev@protonmail.com>
2022-03-19 16:38:05 +0100

    Arguments from terminal

--- logins_gen.py
+++ logins_gen.py
@@ -16,41 +16,43 @@ class LoginGenerator:
 		
 
 	def main(self):
-		file_name = input('Path: ')
-		with open(file_name, 'r') as file1:
-			word_lst = []
-			chars = [' ', '_', '-', '+', '.', ',', '!']
-			lines = file1.readlines()
+		try:
 
-			for line in lines:
-				name, surname, f_name, f_surname = self.spliter(line)
-				word_lst.append(name)
-				word_lst.append(surname)
-				word_lst.append(surname + name)
-				word_lst.append(f_name + surname) 
-				word_lst.append(surname + f_name)
-				word_lst.append(f_surname + name) 
-				word_lst.append(name + f_surname)
+			file_name = sys.argv[1] #input('Path: ')
+			with open(file_name, 'r') as file1:
+				word_lst = []
+				chars = [' ', '_', '-', '+', '.', ',', '!']
+				lines = file1.readlines()
 
-				for char in chars:
-					word_lst.append(name + char + surname)
-					word_lst.append(surname + char + name)
-					word_lst.append(char + surname + name)
-					word_lst.append(char + name + surname)
-					word_lst.append(surname + name + char)
-					word_lst.append(name + surname + char)
-					word_lst.append(f_name + char + surname)
-					word_lst.append(surname + char + f_name)
-					word_lst.append(f_surname + char + name)
-					word_lst.append(name + char + f_surname)
+				for line in lines:
+					name, surname, f_name, f_surname = self.spliter(line)
+					word_lst.append(name)
+					word_lst.append(surname)
+					word_lst.append(surname + name)
+					word_lst.append(f_name + surname) 
+					word_lst.append(surname + f_name)
+					word_lst.append(f_surname + name) 
+					word_lst.append(name + f_surname)
 
-			new_file_name = f'{file_name}_wordlist.txt'
-			with open(new_file_name, 'w') as file2:
-				for word in word_lst:
-					file2.write(word + '\n')
+					for char in chars:
+						word_lst.append(name + char + surname)
+						word_lst.append(surname + char + name)
+						word_lst.append(char + surname + name)
+						word_lst.append(char + name + surname)
+						word_lst.append(surname + name + char)
+						word_lst.append(name + surname + char)
+						word_lst.append(f_name + char + surname)
+						word_lst.append(surname + char + f_name)
+						word_lst.append(f_surname + char + name)
+						word_lst.append(name + char + f_surname)
 
-			
-					
+				new_file_name = f'{file_name[:-4]}_wordlist.txt'
+				with open(new_file_name, 'w') as file2:
+					for word in word_lst:
+						file2.write(word + '\n')
+						
+		except:
+			print('Wrong No path to the file!\nExample: longins_gen.py file_path.txt')
 
 
 if __name__ == '__main__': LoginGenerator() 
